457fe0ada6360916f6e16b4989b7391e1b1260e4
albyariahari <albyriahari@gmail.com>
2018-04-15 16:23:31 +0700

    fix footer

--- view/user/member/insertResponden.php
+++ view/user/member/insertResponden.php
@@ -85,20 +85,7 @@
                 </div>
                 <div class="col-md-2"></div>
             </div>
-            <div class="footer">
-                <div class="copyright">
-                    <p class="pull-left sm-pull-reset">
-                        <span>Copyright <span class="copyright">©</span> 2016 </span>
-                        <span>THEMES LAB</span>.
-                        <span>All rights reserved. </span>
-                    </p>
-                    <p class="pull-right sm-pull-reset">
-                        <span><a href="#" class="m-r-10">Support</a> | <a href="#"
-                                                                          class="m-l-10 m-r-10">Terms of use</a> | <a
-                                    href="#" class="m-l-10">Privacy Policy</a></span>
-                    </p>
-                </div>
-            </div>
+            <?php include_once 'footer.php' ?>
         </div>
         <!-- END PAGE CONTENT -->
     </div>
